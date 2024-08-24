494cefa3b44cbf9f803b623699b9b31375b1bb93
Abhilash107 <abhilash00003108884@gmail.com>
2024-08-24 23:34:28 +0530

    numbers in python

--- 01_basics/intro.py
+++ 01_basics/intro.py
@@ -5,10 +5,25 @@ def code(n):
 
 # code("Hello")
 username = "Abhilash"
-print(username[0]) #A
-print(username[-1]) #h  (last index)
-print(username[len(username)-1]) #h  (last index)
-print(username[1:3]) # bh  1, 2  
+# print(username[0]) #A
+# print(username[-1]) #h  (last index)
+# print(username[len(username)-1]) #h  (last index)
+# print(username[1:3]) # bh  1, 2  
+
+a = 3
+a = "Hello"
+a = 3.14
+#Garbage collection of Number and String takes place a little bit late
+print(a)
+
+
+
+b = 1
+c = 2
+
+b = b + 2
+print(b)
+
 
 
 
