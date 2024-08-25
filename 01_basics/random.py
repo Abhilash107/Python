445851ecb932fd0f2f8e9c454ebc29f71e596a9c
Abhilash107 <abhilash00003108884@gmail.com>
2024-08-25 12:04:27 +0530

    random in python

---
+++ 01_basics/random.py
@@ -0,0 +1,9 @@
+from random import Random
+
+# Create an instance of Random
+random_instance = Random()
+
+l1 = [1, 2, 3, 4, 5]
+
+# Use the choice method on the instance
+print(random_instance.choice(l1))
