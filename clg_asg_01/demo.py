cbc9a139050dafc23e67fa0b32f28c3b4eeedd05
Abhilash107 <abhilash00003108884@gmail.com>
2024-09-25 00:22:34 +0530

    basic syntax

---
+++ clg_asg_01/demo.py
@@ -0,0 +1,13 @@
+#print("Hello \nWorld ")#
+#print("Hello \tWorld ")
+
+name = input("Enter your name: ")
+#print(f"Hello {name}, User no: ",2020392993 + 1 )
+
+#print('int(5.2)', 'truncates 5.2 to', int(5.2))
+#print(''' a  b 'd'  "Abc" ''')
+
+print(type(name))
+# default type of any input from console is STRING
+# so use type casting
+
