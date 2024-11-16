7ba6ad58bf8ac0fd243982d8264808f9542a9eab
Abhilash107 <abhilash00003108884@gmail.com>
2024-11-16 22:31:12 +0530

    hehe

--- clg_asg_01/demo.py
+++ clg_asg_01/demo.py
@@ -11,16 +11,60 @@
 # default type of any input from console is STRING
 # so use type casting
 
-print("hehehe \\abhi ")
-for _ in range(1): 
-    a = int(input("Enter first num: "))
-    b = int(input("Enter second num: "))
-    c = int(input("Enter third num: "))
+# print("hehehe \\abhi ")
+# for _ in range(1): 
+#     a = int(input("Enter first num: "))
+#     b = int(input("Enter second num: "))
+#     c = int(input("Enter third num: "))
 
-max_num = max(a, b, c)
+# max_num = max(a, b, c)
 
-print(f"Max num is: {max_num}")
+# print(f"Max num is: {max_num}")
 
 # Notes: n Python, variables defined inside a for or loop (or any loop) are still in the same scope as the rest of the code that surrounds the loop. Python does not create a separate block scope for loops, so variables declared in a loop are available after the loop ends.
 
 # In Python, if-else blocks also do not create a separate block scope, similar to loops. Any variables defined inside an if, elif, or else block will still be accessible outside of these blocks, provided that the block gets executed.
+# a=int(input('Enter a:'))
+# a=20
+# b=10
+# print(a+b) #30
+
+# a = "nana "
+# b = 8
+# c = a * b
+# print(c, "Batman")
+
+# print('123');
+# print(int("123") + 2)
+
+# n = 10
+# sum_of_number = (n)*(n + 1)//2;
+# print(sum_of_number)
+
+# num = 3141
+# sum_of_digits = 0
+
+# sum_of_digits += num % 10
+# num//=10
+# sum_of_digits += num % 10
+# num//=10
+# sum_of_digits += num % 10
+# num//=10
+# sum_of_digits += num % 10
+# num//=10
+# print(sum_of_digits)
+
+# num1 = int(input("Enter num1: "))
+# num2 = int(input("Enter num2: "))
+# num3 = int(input("Enter num3: "))
+
+# min_num = min(num1, num2, num3)
+# max_num = max(num1, num2, num3)
+# sum_of_num = num1 + num2 + num3
+# middle = sum_of_num - (min_num + max_num)
+
+# print(min_num, middle, max_num)
+
+
+
+
