f8d43abb0d182a4d6419d54e51822f5b968b89f8
Abhilash107 <abhilash00003108884@gmail.com>
2024-12-08 23:37:01 +0530

    dictionary in python

--- Dictionaries/comprehensions.py
+++ Dictionaries/comprehensions.py
@@ -12,7 +12,7 @@ print(months2)
 grades = {'Sue': [98, 87, 94], 'Bob': [84, 95, 91]}
 
 avg_grades = {k: sum(v)/len(v) for k, v in grades.items()}
-print(avg_grades)
+print(avg_grades) 
 
 nums = {n: n**3 for n in range(1,6)}
 print(nums)# automatically assigns keys
