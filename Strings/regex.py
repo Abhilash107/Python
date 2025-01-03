8d0729d71d4fc90982521a68fcb00fececa0d811
Abhilash107 <abhilash00003108884@gmail.com>
2025-01-03 23:32:50 +0530

    vxdv

--- Strings/regex.py
+++ Strings/regex.py
@@ -4,6 +4,10 @@ pattern = '02215'
 print('Match' if re.fullmatch(pattern, '02215') else 'No match')
 print('Match' if re.fullmatch(pattern, '20215') else 'No match')
 
+#metacharacters
+# [] {} () \ * + ^ $ ? . |
+#The \ metacharacter begins each of the predefined character classes, each matching a specific set of characters
+
 
 
 
