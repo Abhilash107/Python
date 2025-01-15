9233afd413b125228aa67f2e2119226e75aa22bf
Abhilash107 <abhilash00003108884@gmail.com>
2025-01-15 18:53:05 +0530

    add new.py for number reversal and power calculation functionality

---
+++ clg_asg_02/new.py
@@ -0,0 +1,9 @@
+num = input("Enter a num:")
+newNum = int(num[::-1])
+print(newNum)
+pow = 1
+newNum1=1
+for i in range (0, len(num), -1):
+    newNum1 = newNum1 ** pow + int(i)
+    pow = pow + 1
+print(newNum1)
