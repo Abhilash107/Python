58c782c1e7d3b7ae9d39749cdb37ce4846196478
Abhilash Mishra <abhilash00003108884@gmail.com>
2026-09-10 12:46:52 +0530

    add: implement basic input/output examples in input_output.py

---
+++ Basics/input_output.py
@@ -0,0 +1,28 @@
+print('hello')
+
+
+print('Abhilash', 10)
+
+# sep
+# Controls the separator between values.
+
+print("A", "b", "c", sep=' | ')
+
+# end
+# Controls what is printed at the end.
+
+print("Hello", end=" ")
+print("World")
+
+# input
+
+# name = input("Enter your name: \t")
+# print(name)
+
+age = input("Enter age: ")
+
+print(type(age))# str
+
+
+# For a decimal:
+price = float(input("Enter price: "))
